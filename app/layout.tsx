0dd5ca9baddef69bf59e5f69e2385693c515c8a0
Jafar Abdulsalam <dhamivibez@gmail.com>
2025-09-12 20:40:35 +0100

    feat(font): use inter as the global font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
